3e683463757abc5e8a383e5ed56e4555f723514e
Angelo Annunziata <ing.annunziata@gmail.com>
2019-10-14 23:13:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,8 @@
-
-// document.onload = init;
-// init();
-
-// function init() {
-// 	const formElem = document.getElementById("survey-form");
-
-// 	formElem.addEventListener("focusin", focusInHandler);
-// 	formElem.addEventListener("focusout", focusOutHandler);
-// }
-
-// function focusInHandler(e) {
-// 	let targetElem = e.target;
-// 	if (targetElem !== e.currentTarget) {
-// 		const labelElem = targetElem.previousElementSibling;
-// 		labelElem.classList.add("label-active");
-// 		console.log("focusIn");
-// 	}
-
-// 	e.stopPropagation();
-// }
-
-// function focusOutHandler(e) {
-// 	let targetElem = e.target;
-// 	if (targetElem !== e.currentTarget) {
-// 		//move label UP
-// 		if (!targetElem.value) {
-// 			const labelElem = targetElem.previousElementSibling;
-// 			labelElem.classList.remove("label-active");
-// 			console.log("focusOut");
-// 		}
-
-// 		//set validity style
-// 		if (targetElem.validity.valid) {
-// 			targetElem.style.borderBottom = "1px solid #0f0";
-// 		} else {
-// 			targetElem.style.borderBottom = "1px solid red";
-// 		}
-// 	}
-
-// 	e.stopPropagation();
-// }
-
-// TAB
 $(document).ready(function() {
+
 	(function($) {
+		
+		// Tabs script
 		$(".tab ul.tabs")
 			.addClass("active")
 			.find("> li:eq(0)")
@@ -72,5 +31,19 @@ $(document).ready(function() {
 
 			g.preventDefault();
 		});
+		// END TABS SCRIPT
+
+		// KEYPRESS EVENT
+		document.addEventListener('keypress', function(e) {
+			console.log(e);
+			var tabsKey = ['1','2','3','4'];
+			if (e.ctrlKey && tabsKey.includes(e.key)) {
+				console.log('in');
+				$(`.tab [data-index=${e.key-1}] a`).trigger( "click" );
+			}
+		});
+		// END KEYPRESS EVENT
+
 	})(jQuery);
+
 });
