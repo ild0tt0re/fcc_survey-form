779fe95dd64730fd21309161bcfbb2c769236f48
Angelo Annunziata <ing.annunziata@gmail.com>
2019-10-21 00:20:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-
 	(function($) {
-		
 		// Tabs script
 		$(".tab ul.tabs")
 			.addClass("active")
@@ -34,16 +32,43 @@ $(document).ready(function() {
 		// END TABS SCRIPT
 
 		// KEYPRESS EVENT
-		document.addEventListener('keypress', function(e) {
+		document.addEventListener("keypress", function(e) {
 			console.log(e);
-			var tabsKey = ['1','2','3','4'];
+			var tabsKey = ["1", "2", "3", "4"];
 			if (e.ctrlKey && tabsKey.includes(e.key)) {
-				console.log('in');
-				$(`.tab [data-index=${e.key-1}] a`).trigger( "click" );
+				console.log("in");
+				$(`.tab [data-index=${e.key - 1}] a`).trigger("click");
 			}
 		});
 		// END KEYPRESS EVENT
-
 	})(jQuery);
+
+/*--------------
+ *  MODAL 
+ *--------------*/
+	// Get the modal
+	var modal = document.getElementById("myModal");
+	// Get the button that opens the modal
+	var btn = document.getElementById("info-icon");
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+	// When the user clicks on the button, open the modal
+	btn.onclick = function() {
+		modal.style.display = "block";
+	};
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	};
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	};
+/*--------------
+ * END MODAL 
+ *--------------*/
+
 
 });
