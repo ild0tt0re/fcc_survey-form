3dda4a421e0cb5d44beb74acdbc146c58c7bce08
Angelo Annunziata <ing.annunziata@gmail.com>
2019-10-22 22:01:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -43,32 +43,75 @@ $(document).ready(function() {
 		// END KEYPRESS EVENT
 	})(jQuery);
 
-/*--------------
- *  MODAL 
- *--------------*/
+	/*--------------
+	 *  MODAL
+	 *--------------*/
 	// Get the modal
 	var modal = document.getElementById("myModal");
 	// Get the button that opens the modal
 	var btn = document.getElementById("info-icon");
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
+
+	var modalContent = modal.querySelector(".modal-content");
+	function hideModalLayer() {
+		modal.style.display = "none";
+	}
 	// When the user clicks on the button, open the modal
 	btn.onclick = function() {
+		modal.removeEventListener("transitionend", hideModalLayer);
 		modal.style.display = "block";
+		setTimeout(() => modalContent.classList.add("modal-show"), 0.1);
 	};
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
-		modal.style.display = "none";
+		removeModal();
 	};
 	// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
 		if (event.target == modal) {
-			modal.style.display = "none";
+			removeModal();
 		}
 	};
-/*--------------
- * END MODAL 
- *--------------*/
 
+	function removeModal() {
+		modal.addEventListener("transitionend", hideModalLayer);
+		modalContent.classList.remove("modal-show");
+	}
 
+	/*---
+	 * END MODAL
+	 *--------------*/
+
+	/*---------------
+	  SUBMIT BUTTON
+	 ----------------- */
+
+	document.querySelector("button").addEventListener("click", function(e) {
+		e.preventDefault();
+		var invalidFields = document.querySelectorAll("input:invalid");
+		var modalFormValidity = document.getElementsByClassName(
+			"modal-form-validity"
+		)[0];
+
+		if (invalidFields.length === 0) {
+			modalFormValidity.textContent =
+				"SUCCESS: all fields have been validated.";
+			modalFormValidity.classList.add("success");
+		} else {
+			Array.from(invalidFields).forEach(function(invalidField) {
+				console.error("Field ", invalidField.name, " is invalid!");
+			});
+			modalFormValidity.textContent = "ERROR: Some fields are invalid...";
+			modalFormValidity.classList.add("error");
+		}
+
+		setTimeout(function() {
+			modalFormValidity.classList.remove("success", "error");
+		}, 3000);
+	});
+
+	/*---
+	  END SUBMIT BUTTON
+	 ----------------- */
 });
